313984aa2503d825a31b9d436afeb0b9d37272d9
shahad_jeza <shoodialamri@gmail.com>
2024-11-04 04:00:06 +0300

    Added environment variables and error handling

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 def create_app():
     # Create the Flask application instance
-    app = Flask(__name__, template_folder='../templates',static_folder='../static')
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
     # Optional: Additional configurations can go here
     # e.g., app.config['SECRET_KEY'] = 'your_secret_key'
@@ -16,3 +16,6 @@ def create_app():
     app.register_blueprint(main)
 
     return app
+
+# Create an instance of the app when the module is imported
+app = create_app()
